437c9ea8effdd3b10cb6fc993f2d62249edab05f
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-12-21 13:59:23 +0100

    init php version of project and import data

---
+++ v-php/index.php
@@ -0,0 +1,16 @@
+<?php require_once __DIR__ . '/api/database.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>v-php</title>
+</head>
+
+<body>
+</body>
+
+</html>

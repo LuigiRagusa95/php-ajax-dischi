9f237055767642e9574142714468d0e785fbea84
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-12-21 14:33:12 +0100

    add styles and loop in array with php

--- v-php/index.php
+++ v-php/index.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . '/api/database.php'; ?>
+<?php require_once __DIR__ . '/data/database.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +11,35 @@
 </head>
 
 <body>
+    <header>
+        <div class="container">
+            <div>
+                <div class="logo">
+                    <img src="./img/logo.png" alt="image description">
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <ul>
+                <?php foreach ($database as $key => $value) : ?>
+                    <li class="card">
+                        <div class="card-image">
+                            <div>
+                                <img src="<?php echo $value['poster'] ?>" alt="image description">
+                            </div>
+                        </div>
+                        <h3 class="card-title"> <?php echo $value['title'] ?> </h3>
+                        <p class="card-author"> <?php echo $value['author'] ?> </p>
+                        <h4 class="card-year"> <?php echo $value['year'] ?> </h4>
+                        <div class="card-genre"> <?php echo $value['genre'] ?> </div>
+                    </li>
+                <?php endforeach ?>
+
+            </ul>
+        </div>
+    </main>
 </body>
 
 </html>
